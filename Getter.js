422f9dd41ab45294a93abf21ad36d330ddf26f9d
Eugene362624 <63005581+Eugene362624@users.noreply.github.com>
2021-06-11 15:06:14 +0300

    Update Getter.js

--- Getter.js
+++ Getter.js
@@ -7,9 +7,9 @@ const Poster = () =>
   axios.post(`https://jogtracker.herokuapp.com/api/v1/test/echo`).then((res) => res.data.timestamp);
 
 const Putter = () =>
-  axios.post(`https://jogtracker.herokuapp.com/api/v1/test/echo`).then((res) => res.data.timestamp);
+  axios.put(`https://jogtracker.herokuapp.com/api/v1/test/echo`).then((res) => res.data.timestamp);
 
-const Delitter = () =>
-  axios.post(`https://jogtracker.herokuapp.com/api/v1/test/echo`).then((res) => res.data.timestamp);
+const Deleter = () =>
+  axios.delete(`https://jogtracker.herokuapp.com/api/v1/test/echo`).then((res) => res.data.timestamp);
 
 export {Getter, Poster, Putter, Delitter};
